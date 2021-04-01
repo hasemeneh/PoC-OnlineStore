033b13e421eb1605780e13b860e1c6c5b28e7c7b
hasemeneh <rivaldi.julviar@tokopedia.com>
2021-04-02 00:31:35 +0700

    update protoc

--- svc/grpc/warehouse/protos/warehouse.pb.go
+++ svc/grpc/warehouse/protos/warehouse.pb.go
@@ -35,7 +35,7 @@ func (m *ProductRequest) Reset()         { *m = ProductRequest{} }
 func (m *ProductRequest) String() string { return proto.CompactTextString(m) }
 func (*ProductRequest) ProtoMessage()    {}
 func (*ProductRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_warehouse_193917cdbb7c0d91, []int{0}
+	return fileDescriptor_warehouse_cdf0fe9ada95e2f0, []int{0}
 }
 func (m *ProductRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_ProductRequest.Unmarshal(m, b)
@@ -82,7 +82,7 @@ func (m *ProductResponse) Reset()         { *m = ProductResponse{} }
 func (m *ProductResponse) String() string { return proto.CompactTextString(m) }
 func (*ProductResponse) ProtoMessage()    {}
 func (*ProductResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_warehouse_193917cdbb7c0d91, []int{1}
+	return fileDescriptor_warehouse_cdf0fe9ada95e2f0, []int{1}
 }
 func (m *ProductResponse) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_ProductResponse.Unmarshal(m, b)
@@ -125,8 +125,8 @@ func (m *ProductResponse) GetStatus() int64 {
 
 type ClaimRequest struct {
 	Products             []*ProductRequest `protobuf:"bytes,1,rep,name=products,proto3" json:"products,omitempty"`
-	OrderID              int64             `protobuf:"varint,2,opt,name=orderID,proto3" json:"orderID,omitempty"`
-	CartID               int64             `protobuf:"varint,3,opt,name=cartID,proto3" json:"cartID,omitempty"`
+	CartID               int64             `protobuf:"varint,2,opt,name=cartID,proto3" json:"cartID,omitempty"`
+	OrderID              int64             `protobuf:"varint,3,opt,name=orderID,proto3" json:"orderID,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}          `json:"-"`
 	XXX_unrecognized     []byte            `json:"-"`
 	XXX_sizecache        int32             `json:"-"`
@@ -136,7 +136,7 @@ func (m *ClaimRequest) Reset()         { *m = ClaimRequest{} }
 func (m *ClaimRequest) String() string { return proto.CompactTextString(m) }
 func (*ClaimRequest) ProtoMessage()    {}
 func (*ClaimRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_warehouse_193917cdbb7c0d91, []int{2}
+	return fileDescriptor_warehouse_cdf0fe9ada95e2f0, []int{2}
 }
 func (m *ClaimRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_ClaimRequest.Unmarshal(m, b)
@@ -163,16 +163,16 @@ func (m *ClaimRequest) GetProducts() []*ProductRequest {
 	return nil
 }
 
-func (m *ClaimRequest) GetOrderID() int64 {
+func (m *ClaimRequest) GetCartID() int64 {
 	if m != nil {
-		return m.OrderID
+		return m.CartID
 	}
 	return 0
 }
 
-func (m *ClaimRequest) GetCartID() int64 {
+func (m *ClaimRequest) GetOrderID() int64 {
 	if m != nil {
-		return m.CartID
+		return m.OrderID
 	}
 	return 0
 }
@@ -188,7 +188,7 @@ func (m *ClaimResponse) Reset()         { *m = ClaimResponse{} }
 func (m *ClaimResponse) String() string { return proto.CompactTextString(m) }
 func (*ClaimResponse) ProtoMessage()    {}
 func (*ClaimResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_warehouse_193917cdbb7c0d91, []int{3}
+	return fileDescriptor_warehouse_cdf0fe9ada95e2f0, []int{3}
 }
 func (m *ClaimResponse) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_ClaimResponse.Unmarshal(m, b)
@@ -327,9 +327,9 @@ var _Warehouse_serviceDesc = grpc.ServiceDesc{
 	Metadata: "warehouse.proto",
 }
 
-func init() { proto.RegisterFile("warehouse.proto", fileDescriptor_warehouse_193917cdbb7c0d91) }
+func init() { proto.RegisterFile("warehouse.proto", fileDescriptor_warehouse_cdf0fe9ada95e2f0) }
 
-var fileDescriptor_warehouse_193917cdbb7c0d91 = []byte{
+var fileDescriptor_warehouse_cdf0fe9ada95e2f0 = []byte{
 	// 268 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x2f, 0x4f, 0x2c, 0x4a,
 	0xcd, 0xc8, 0x2f, 0x2d, 0x4e, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x84, 0x0b, 0x28,
@@ -340,12 +340,12 @@ var fileDescriptor_warehouse_193917cdbb7c0d91 = []byte{
 	0x06, 0x81, 0xc4, 0x8b, 0x4b, 0x12, 0x4b, 0x4a, 0x8b, 0x25, 0x98, 0x21, 0xe2, 0x10, 0x9e, 0x52,
 	0x39, 0x17, 0x8f, 0x73, 0x4e, 0x62, 0x66, 0x2e, 0xcc, 0x99, 0xa6, 0x5c, 0x1c, 0x50, 0xc3, 0x8a,
 	0x25, 0x18, 0x15, 0x98, 0x35, 0xb8, 0x8d, 0x24, 0xf5, 0x10, 0xfe, 0x44, 0xf5, 0x53, 0x10, 0x5c,
-	0xa9, 0x90, 0x04, 0x17, 0x7b, 0x7e, 0x51, 0x4a, 0x6a, 0x91, 0xa7, 0x0b, 0xd4, 0x5e, 0x18, 0x17,
-	0x64, 0x71, 0x72, 0x62, 0x11, 0xc8, 0xad, 0x50, 0x8b, 0x21, 0x3c, 0x25, 0x77, 0x2e, 0x5e, 0xa8,
+	0x29, 0xc8, 0xf8, 0xe4, 0xc4, 0x22, 0x90, 0x8b, 0xa0, 0xd6, 0x42, 0x78, 0x42, 0x12, 0x5c, 0xec,
+	0xf9, 0x45, 0x29, 0xa9, 0x45, 0x9e, 0x2e, 0x50, 0x7b, 0x61, 0x5c, 0x25, 0x77, 0x2e, 0x5e, 0xa8,
 	0xc5, 0x50, 0x7f, 0x99, 0x61, 0xd8, 0x2c, 0x85, 0xcd, 0x66, 0x88, 0x6a, 0x84, 0xd5, 0x46, 0xb3,
 	0x18, 0xb9, 0x38, 0xc3, 0x61, 0xea, 0x84, 0x3c, 0xb8, 0x78, 0xc3, 0x12, 0x73, 0x32, 0x53, 0x12,
 	0x4b, 0x52, 0x83, 0x4b, 0xf2, 0x93, 0xb3, 0x85, 0x70, 0x3b, 0x5f, 0x0a, 0x8f, 0xf9, 0x4a, 0x0c,
 	0x42, 0xce, 0xd0, 0x90, 0x81, 0xca, 0x08, 0x89, 0x23, 0xa9, 0x46, 0x0e, 0x32, 0x29, 0x09, 0x4c,
 	0x09, 0x98, 0x21, 0x4e, 0xdc, 0x51, 0x88, 0x44, 0x91, 0xc4, 0x06, 0x4e, 0x26, 0xc6, 0x80, 0x00,
-	0x00, 0x00, 0xff, 0xff, 0x36, 0x58, 0xb4, 0x63, 0x39, 0x02, 0x00, 0x00,
+	0x00, 0x00, 0xff, 0xff, 0x21, 0xfb, 0x03, 0x55, 0x39, 0x02, 0x00, 0x00,
 }
